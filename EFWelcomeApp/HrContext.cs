1790e93106a02dbd20162e5d1565905e60b97216
maxefimov <efimov.top.academy@gmail.com>
2024-05-16 21:09:13 +0300

    Добавьте файлы проекта.

---
+++ EFWelcomeApp/HrContext.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFWelcomeApp
+{
+    public class HrContext : DbContext
+    {
+        public DbSet<Employee> Employees { get; set; }
+        //public DbSet<Company> Companys { get; set; }
+
+        StreamWriter logWriter = new("log.txt", true);
+
+        public HrContext()
+        {
+            
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+
+            var config = new ConfigurationBuilder()
+                            .AddJsonFile("hr_settings.json")
+                            .SetBasePath(Directory.GetCurrentDirectory())
+                            .Build();
+
+
+            optionsBuilder.UseSqlServer(config.GetConnectionString("DefaultConnection"));
+            optionsBuilder.LogTo(logWriter.WriteLine, new[] { RelationalEventId.CommandExecuted });
+        }
+
+        public override void Dispose()
+        {
+            base.Dispose();
+            logWriter.Dispose();
+        }
+
+        public override async ValueTask DisposeAsync()
+        {
+            await base.DisposeAsync();
+            await logWriter.DisposeAsync();
+        }
+    }
+}
---
+++ EFWelcomeApp/Program.cs
@@ -0,0 +1,65 @@
+﻿using EFWelcomeApp;
+using Microsoft.EntityFrameworkCore;
+
+//using (HrContext context = new())
+//{
+//    context.Database.EnsureDeleted();
+//    //context.Database.EnsureCreated();
+//}
+
+//using(HrContext context = new())
+//{
+//    //context.Database.Migrate();
+//    await context.Database.MigrateAsync();
+//}
+
+
+
+// C - Create
+using (HrContext context = new())
+{
+    Company yandex = new Company() { Title = "Yandex" };
+    Company ozon = new Company() { Title = "Ozon" };
+
+    Employee bob = new Employee() { Name = "Bobby", Age = 28, Company = ozon };
+    Employee ann = new Employee() { Name = "Anna", Age = 32, Company = yandex };
+
+    //context.Employees.AddRange(bob, ann);
+    await context.Employees.AddRangeAsync(bob, ann);
+    await context.SaveChangesAsync();
+}
+
+// U - Update
+using (HrContext context = new())
+{
+    Employee? employee = context.Employees.FirstOrDefault();
+    if (employee is not null)
+    {
+        employee.Name = "Bobby Fisher";
+        employee.Age = 40;
+
+        context.SaveChanges();
+    }
+}
+
+// D - Delete
+//using (HrContext context = new())
+//{
+//    Employee? employee = context.Employees.FirstOrDefault();
+//    if (employee is not null)
+//    {
+//        context.Employees.Remove(employee);
+
+//        context.SaveChanges();
+//    }
+//}
+
+
+// R - Read
+using (HrContext context = new())
+{
+    var employees = await context.Employees.ToListAsync();
+    Console.WriteLine("Employees:");
+    foreach (var e in employees)
+        Console.WriteLine($"{e.Id}: {e.Name} ({e.Age})");
+}
